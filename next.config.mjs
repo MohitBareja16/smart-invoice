3fe81f4963f9aee189b84d21634f8401600982bb
Mohit Bareja <75233239+MohitBareja16@users.noreply.github.com>
2025-07-25 20:33:27 +0530

    Update next.config.mjs
    MONGODB_URI

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+  // Enable PDF rendering
+  experimental: {
+    serverComponentsExternalPackages: ['@react-pdf/renderer'],
+  },
 export default nextConfig;
